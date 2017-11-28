5f0aa99b462364e7a4f2d5219f4d63132df2748c
kac2z7 <kac2z7@mail.missouri.edu>
2017-11-28 10:15:40 -0600

    Create color.swift

---
+++ Colors2/Colors2/color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors2
+//
+//  Created by Keith
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    let name: String
+    let uicolor: UIColor
+}
